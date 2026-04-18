04e8e6c25a8b15b86ea16a5cf0e730778972381d
ddsurvivor <63942441+ddsurvivor@users.noreply.github.com>
2026-04-18 11:59:48 +0800

    优化战斗ui显示位置

--- Assets/Scripts/Battle/UI/PieceStatePanel.cs
+++ Assets/Scripts/Battle/UI/PieceStatePanel.cs
@@ -15,6 +15,10 @@ public class PieceStatePanel : UIPanel
     public Image healthBar;
     //public Image mpBar;
     public Image manaBar;
+
+    public Transform upRoot;
+    public float upRootBaseY = 50f; // upRoot的基础Y位置
+    public float upRootScaleFactor = 50f; // upRoot随相机缩放的调整系数
     //public Text manaText;
 
     public void Update()
@@ -24,6 +28,10 @@ public class PieceStatePanel : UIPanel
             // 计算棋子在屏幕中的位置，更新行动面板的位置
             Vector3 screenPos = Camera.main.WorldToScreenPoint(pc.transform.position);
             transform.position = screenPos + new Vector3(0, 100, 0);
+            
+            // upRoot 根据当前相机的视角缩放来调整位置，相机缩放越大，upRoot离屏幕中心越远，反之亦然
+            float scaleFactor = 1f/ Camera.main.orthographicSize; // 根据相机的正交大小计算缩放因子
+            upRoot.localPosition = new Vector3(0,  upRootBaseY + upRootScaleFactor * scaleFactor, 0);
         }
     }
 
