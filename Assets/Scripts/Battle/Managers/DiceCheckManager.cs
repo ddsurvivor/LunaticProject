f3e62f7a953ffeefa319b9c54ede01d86cd0453b
ddsurvivor <63942441+ddsurvivor@users.noreply.github.com>
2026-04-30 23:39:26 +0800

    字体修改

---
+++ Assets/Scripts/Battle/Managers/DiceCheckManager.cs
@@ -0,0 +1,45 @@
+
+    using UnityEngine;
+
+    public class DiceCheckManager: MonoBehaviour
+    {
+        
+        /// <summary>
+        /// 检定功能函数：模式识别对抗
+        /// </summary>
+        /// <param name="attacker">攻击方棋子</param>
+        /// <param name="defender">目标方棋子</param>
+        /// <returns>检定结果</returns>
+        public CheckResult ModeRecognitionCheck(PieceController attacker, PieceController defender)
+        {
+            // 假设属性名为 ModeRecognition 和 CounterAttribute
+            // 攻击方模式识别属性加成
+            int modeRecognition = attacker.playerData.RECOGNITION;
+            // 敌人对抗
+            int counterAttr = 0;// defender.unitAttrCenter.buffAttrDic[BuffAttrType.CounterAttribute];
+
+            // 投掷3D6
+            int diceSum = Random.Range(1, 7) + Random.Range(1, 7) + Random.Range(1, 7);
+
+            float total = modeRecognition + diceSum;
+
+            if (total < counterAttr)
+            {
+                return CheckResult.DamageReduced;
+            }
+            else if (total >= 1.5f * counterAttr)
+            {
+                return CheckResult.MustCrit;
+            }
+            else // total > counterAttr && total < 1.5 * counterAttr
+            {
+                return CheckResult.DamageIncreased;
+            }
+        }
+    }
+    public enum CheckResult
+    {
+        DamageReduced,   // 伤害减少
+        DamageIncreased, // 伤害增加
+        MustCrit         // 必定暴击
+    }
