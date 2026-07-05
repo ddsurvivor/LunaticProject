86c92697455225dc75cd94c4384a6d87e297eade
octbanal-jpg <octbanal@gmail.com>
2026-07-05 15:44:37 +0800

    修改部分数值平衡

--- Assets/Scripts/Battle/Tool/GameConst.cs
+++ Assets/Scripts/Battle/Tool/GameConst.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public static class GameConst
 {
-    public const int attackBurstCharge = 20;
-    //public const int skillBurstCharge = 40;
-    public const int hurtBurstCharge = 10;
-    public const float burstDamageRate = 1.5f; // 聚能发动时的伤害加成比例
-    public const float burstAddDamageRate = 0.3f; // 聚能发动后增加的伤害比例
+    public const int attackBurstCharge = 10;
+    //public const int skillBurstCharge = 15;
+    public const int hurtBurstCharge = 5;
+    public const float burstDamageRate = 1.2f; // 聚能发动时的伤害加成比例
+    public const float burstAddDamageRate = 0.2f; // 聚能发动后增加的伤害比例
     public const int enemySkillRate = 35;// 敌人使用技能的概率，百分比
     public const int initialCoins = 3000; // 初始金币数量
     
